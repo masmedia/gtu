56d2a7864e3d8c7e329adff9593add8e538480c9
masmedia <mustafa.sari.media@gmail.com>
2018-01-19 02:29:41 +0300

    Made Monte_Carlo_Simulation class portable

--- elec631/20171116_0107_duck_shooting/include/Monte_Carlo_Simulation.hpp
+++ elec631/20171116_0107_duck_shooting/include/Monte_Carlo_Simulation.hpp
@@ -1,27 +1,27 @@
 #ifndef MONTE_CARLO_SIMULATION_H_20171126_1728
 #define MONTE_CARLO_SIMULATION_H_20171126_1728
-
+#include <iostream>
 namespace Monte_Carlo_Simulation_NS{
     class Monte_Carlo_Simulation{
         private:
-            int noTests_;
-            int noHunters_;
-            int spacing_;
+            uint64_t noTests_;
+            uint64_t noHunters_;
+            uint64_t spacing_;
 
         public:
             /** Default constructor */
             Monte_Carlo_Simulation();
 
             /** Three Argument Constructor*/
-            Monte_Carlo_Simulation(const int, const int, const int);
+            Monte_Carlo_Simulation(const uint64_t, const uint64_t, const uint64_t);
 
             /** Default destructor */
             ~Monte_Carlo_Simulation();
 
             /** Getters */
-            const int NoTests_() const;
-            const int NoHunters_() const;
-            const int Spacing_() const;
+            const uint64_t NoTests_() const;
+            const uint64_t NoHunters_() const;
+            const uint64_t Spacing_() const;
 
             /** Simulation */
             void simulate();
--- elec631/20171116_0107_duck_shooting/src/Monte_Carlo_Simulation.cpp
+++ elec631/20171116_0107_duck_shooting/src/Monte_Carlo_Simulation.cpp
@@ -11,15 +11,15 @@ Monte_Carlo_Simulation_NS::Monte_Carlo_Simulation::Monte_Carlo_Simulation(): noT
 //Constructor
 Monte_Carlo_Simulation_NS::
     Monte_Carlo_Simulation::
-        Monte_Carlo_Simulation(const int nTest, const int nHunt, const int spa): noTests_(10000), noHunters_(10), spacing_(31){}
+        Monte_Carlo_Simulation(const uint64_t nTest, const uint64_t nHunt, const uint64_t spa): noTests_(nTest), noHunters_(nHunt), spacing_(spa){}
 
 //Destructor
 Monte_Carlo_Simulation_NS::Monte_Carlo_Simulation::~Monte_Carlo_Simulation(){}
 
 //Getters
-const int Monte_Carlo_Simulation_NS::Monte_Carlo_Simulation::NoTests_() const{    return noTests_;   }
-const int Monte_Carlo_Simulation_NS::Monte_Carlo_Simulation::NoHunters_() const { return noHunters_; }
-const int Monte_Carlo_Simulation_NS::Monte_Carlo_Simulation::Spacing_() const {   return spacing_;   }
+const uint64_t Monte_Carlo_Simulation_NS::Monte_Carlo_Simulation::NoTests_() const{    return noTests_;   }
+const uint64_t Monte_Carlo_Simulation_NS::Monte_Carlo_Simulation::NoHunters_() const { return noHunters_; }
+const uint64_t Monte_Carlo_Simulation_NS::Monte_Carlo_Simulation::Spacing_() const {   return spacing_;   }
 
 //Do the simulation
 void Monte_Carlo_Simulation_NS::Monte_Carlo_Simulation::simulate(){
@@ -28,22 +28,22 @@ void Monte_Carlo_Simulation_NS::Monte_Carlo_Simulation::simulate(){
 
     std::mt19937 mt( time(0) );
 
-    Linear_Spacing lam(3, 8, Spacing_());      //lam.printVector();
-    Linear_Spacing p(0.05, 0.95, Spacing_());  //p.printVector();
-    Matrix res(Spacing_(), Spacing_(), 0);     //res.printMatrix();
+    Linear_Spacing lam(3, 8, Spacing_());      //lam.pruint64_tVector();
+    Linear_Spacing p(0.05, 0.95, Spacing_());  //p.pruint64_tVector();
+    Matrix res(Spacing_(), Spacing_(), 0);     //res.pruint64_tMatrix();
 
-    int numProcessor = omp_get_num_procs();
+    uint64_t numProcessor = omp_get_num_procs();
     #pragma omp parallel for num_threads(numProcessor)
-    for(int kk = 0; kk < Spacing_(); kk++){
-    //for(int kk = 0; kk < 4; kk++){
-        for (int ll = 0; ll < Spacing_(); ll++ ){
-        //for (int ll = 0; ll < 3; ll++ ){
+    for(uint64_t kk = 0; kk < Spacing_(); kk++){
+    //for(uint64_t kk = 0; kk < 4; kk++){
+        for (uint64_t ll = 0; ll < Spacing_(); ll++ ){
+        //for (uint64_t ll = 0; ll < 3; ll++ ){
             std::vector<double> tests(NoTests_(), 0);  //Initialize column vector of size (1,NoTests_()=10000) with zeros
             double totalHitDuck = 0;
 
-            for (int mm = 0; mm< NoTests_(); mm++){
-            //for (int mm = 0; mm< 6; mm++){
-                std::poisson_distribution<int> pd(lam(kk));
+            for (uint64_t mm = 0; mm< NoTests_(); mm++){
+            //for (uint64_t mm = 0; mm< 6; mm++){
+                std::poisson_distribution<uint64_t> pd(lam(kk));
                 Matrix tmp(pd(mt), NoHunters_(), -99);
                 //tmp.printMatrix();
 
@@ -51,12 +51,12 @@ void Monte_Carlo_Simulation_NS::Monte_Carlo_Simulation::simulate(){
                     tests.at(mm) = 0;
                 }
                 else{
-                    std::map<int, int> targets;
-                    std::uniform_int_distribution<int> distribution(1, tmp.R_());
+                    std::map<uint64_t, uint64_t> targets;
+                    std::uniform_int_distribution<uint64_t> distribution(1, tmp.R_());
 
-                    for(int vv = 0; vv<NoHunters_(); vv++){
-                        int temp_vv = distribution(mt);
-                        std::map<int, int>::iterator it = targets.find(temp_vv);
+                    for(uint64_t vv = 0; vv<NoHunters_(); vv++){
+                        uint64_t temp_vv = distribution(mt);
+                        std::map<uint64_t, uint64_t>::iterator it = targets.find(temp_vv);
                         if( it == targets.end()){
                             targets.insert( std::make_pair(temp_vv, 1) );
                         }
@@ -65,12 +65,12 @@ void Monte_Carlo_Simulation_NS::Monte_Carlo_Simulation::simulate(){
                         }
                     }
 
-                    for( int nn=0; nn<tmp.R_(); nn++){
+                    for( uint64_t nn=0; nn<tmp.R_(); nn++){
                         for(auto it=targets.begin(); it!=targets.end(); it++){
                             if(it->first == nn){
-                                int number = it->second;
+                                uint64_t number = it->second;
                                 std::uniform_real_distribution<double> real_distr(0, 1);
-                                for (int jkl=0; jkl<number; jkl++){
+                                for (uint64_t jkl=0; jkl<number; jkl++){
                                     tmp.M_( nn, jkl, real_distr(mt) );
                                 }
                                 number = 0;
@@ -82,8 +82,8 @@ void Monte_Carlo_Simulation_NS::Monte_Carlo_Simulation::simulate(){
                 //tmp.printMatrix();
                 //std::cout << "p(ll):" << p(ll) << "\n";
                 double hitDuck = 0;
-                for (int in = 0; in < tmp.R_(); in++){
-                    for (int ij = 0; ij< tmp.C_(); ij++){
+                for (uint64_t in = 0; in < tmp.R_(); in++){
+                    for (uint64_t ij = 0; ij< tmp.C_(); ij++){
                         if( tmp.M_().at(in).at(ij) == -99 ){
                             tmp.M_(in, ij, 0);
                         }
@@ -100,7 +100,7 @@ void Monte_Carlo_Simulation_NS::Monte_Carlo_Simulation::simulate(){
 
                 //tmp.printMatrix();
                 //Clear tmp
-                for(int in=0; in<tmp.numel(); in++){
+                for(uint64_t in=0; in<tmp.numel(); in++){
                     tmp(in, 0);
                 }
                 totalHitDuck += hitDuck;
